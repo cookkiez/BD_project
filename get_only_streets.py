f7770aef874df79df455b775138d522ce80e03ec
cookkiez <aj8977@student.uni-lj.si>
2023-05-20 10:42:24 +0200

    Got data from api and stored to parquet file

--- get_only_streets.py
+++ get_only_streets.py
@@ -17,17 +17,17 @@ def test(row):
 
 def write_parquet(df):
     print("WRITE", df.head())
-    # write('/d/hpc/home/aj8977/Project/streets.parquet', df)
-    df.to_parquet('/d/hpc/home/aj8977/Project/streets.parquet', overwrite=True, engine='fastparquet', ignore_divisions=True, write_index=False)
+    df.to_parquet('/d/hpc/home/aj8977/Project/streets_full.parquet', engine='pyarrow')
 
 def get_data_from_api_dask(ddf):
     import time
     start_time = time.time()
     lats = []
     lngs = []
-    cluster = SLURMCluster(cores=4, memory="5000M",
-                            scheduler_options={"dashboard_address": f":{portdash}"})
-    cluster.scale(5)
+    cluster = SLURMCluster(cores=20, memory="5000M",
+                           scheduler_options={"dashboard_address": f":{portdash}"},
+                           walltime='03:00:00')
+    cluster.scale(10)
     print(cluster)
     client = Client(cluster)
     print(client)
@@ -43,24 +43,21 @@ def get_data_from_api_dask(ddf):
         lngs.append(str(lng))
         cnt += 1
     print((time.time() - start_time))
-    # chunks = ddf.map_partitions(lambda x: len(x)).compute().to_numpy()
-    # la = da.from_array(lats, chunks=tuple(chunks))
-    # ln = da.from_array(lngs, chunks=tuple(chunks))
     la = pd.DataFrame(lats, columns=["lat"])
     ln = pd.DataFrame(lngs, columns=["lng"])
     ddf['lat'] = la["lat"]
     ddf['lng'] = ln["lng"]
-    # cluster.close()
-    # client.close()
     return ddf
 
+# Change file name to dataset.parquet if you want new data capture
+ddf = dd.read_parquet('/d/hpc/home/aj8977/Project/streets_full.parquet', 
+                         engine='pyarrow',)
+print(ddf.head())
 
-ddf = dd.read_parquet('/d/hpc/home/aj8977/Project/dataset_small.parquet', 
-                         engine='fastparquet',)
-
-streets_ddf = ddf.drop_duplicates(subset='street_name')
-df = dd.from_pandas(pd.DataFrame(data=streets_ddf["street_name"], columns=['street_name']), npartitions=1)
-print(df.head())
-df = get_data_from_api_dask(df)
-print(df.head())
-write_parquet(df)
+# Uncomment to run data capture
+# streets_ddf = ddf.drop_duplicates(subset='street_name')
+# df = dd.from_pandas(pd.DataFrame(data=streets_ddf["street_name"], columns=['street_name']), npartitions=1)
+# print(df.head())
+# df = get_data_from_api_dask(df)
+# print(df.head())
+# write_parquet(df)
